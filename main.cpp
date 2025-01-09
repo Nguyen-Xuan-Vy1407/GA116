c0a5728de7e6cc2350ed30ad14c7da1e9125724c
XuanVy <pc@DESKTOP-KJBOUAL>
2025-01-09 20:12:02 +0700

    update 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 #using namespace std;
 int main(){
 	cout<<"Hello world";
-	
+	int a,b;
+	cout<<"Nhap gia tri a ";
+	cin >>a;
+	cout<<"Nhap gia tri b ";
+	cin>>b;
+
+	cout>>"Tong a va b la "<<a+b<<endl;
+	 
+
 }
