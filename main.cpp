0f5166fa99fbbcbf4388ce06dd99cae17a57d735
XuanVy <pc@DESKTOP-KJBOUAL>
2025-01-09 20:26:10 +0700

    update 2

--- main.cpp
+++ main.cpp
@@ -10,5 +10,13 @@ int main(){
 
 	cout>>"Tong a va b la "<<a+b<<endl;
 	 
+	cout>>"Hieu a va b la "<<a-b<<endl;
 
+	double c,d;
+	cout<<"Nhap gia tri c ";
+	cin>>c;
+	cout<<"Nhap gia tri d ";
+	cin>>d;
+	cout<<"Tich c nhan d la "<<c*d<<endl;
+	cout<<"Thuong a chia b la "<<c/b<<endl;
 }
